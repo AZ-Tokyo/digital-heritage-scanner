c4466d28bbbeafecfb783d059520c8264b027226
re4lity <emiliataanjyd@gmail.com>
2026-01-13 15:45:11 +0900

    chore: プロジェクト基盤セットアップ
    - Vite + TypeScript + @crxjs/vite-plugin 構成
    - デジタル庁デザインシステム (Tailwind CSS) 導入
    - ESLint, Prettier, Vitest 設定

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+import themePlugin from '@digital-go-jp/tailwind-theme-plugin'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./popup.html', './src/**/*.{ts,tsx}'],
+    plugins: [themePlugin],
+}
